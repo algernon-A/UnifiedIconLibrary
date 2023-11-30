745059f90d85bcecd780f8aea0bfcbf63e0c56d7
algernon-A <aeneas@aloku.net>
2023-11-30 15:20:25 +1100

    Add project files.

---
+++ Code/Mod.cs
@@ -0,0 +1,105 @@
+﻿// <copyright file="Mod.cs" company="algernon (K. Algernon A. Sheppard)">
+// Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// </copyright>
+
+namespace LineTool
+{
+    using System.IO;
+    using System.Reflection;
+    using Colossal.IO.AssetDatabase;
+    using Colossal.Logging;
+    using Game;
+    using Game.Modding;
+    using Game.SceneFlow;
+    using Game.UI;
+
+    /// <summary>
+    /// The base mod class for instantiation by the game.
+    /// </summary>
+    public sealed class Mod : IMod
+    {
+        /// <summary>
+        /// The mod's default name.
+        /// </summary>
+        public const string ModName = "Unified Icon Library";
+
+        // Mod assembly path cache.
+        private string s_assemblyPath = null;
+
+        /// <summary>
+        /// Gets the active instance reference.
+        /// </summary>
+        public static Mod Instance { get; private set; }
+
+        /// <summary>
+        /// Gets the mod directory file path of the currently executing mod assembly.
+        /// </summary>
+        public string AssemblyPath
+        {
+            get
+            {
+                // Update cached path if the existing one is invalid.
+                if (string.IsNullOrWhiteSpace(s_assemblyPath))
+                {
+                    // No path cached - find current executable asset.
+                    string assemblyName = Assembly.GetExecutingAssembly().FullName;
+                    ExecutableAsset modAsset = AssetDatabase.global.GetAsset(SearchFilter<ExecutableAsset>.ByCondition(x => x.definition?.FullName == assemblyName));
+                    if (modAsset is null)
+                    {
+                        Log.Error("mod executable asset not found");
+                        return null;
+                    }
+
+                    // Update cached path.
+                    s_assemblyPath = Path.GetDirectoryName(modAsset.GetMeta().path);
+                }
+
+                // Return cached path.
+                return s_assemblyPath;
+            }
+        }
+
+        /// <summary>
+        /// Gets the mod's active log.
+        /// </summary>
+        internal static ILog Log { get; private set; }
+
+        /// <summary>
+        /// Called by the game when the mod is loaded.
+        /// </summary>
+        public void OnLoad()
+        {
+            // Set instance reference.
+            Instance = this;
+
+            // Initialize logger.
+            Log = LogManager.GetLogger(ModName);
+            Log.Info("setting logging level to Debug");
+            Log.effectivenessLevel = Level.Debug;
+
+            Log.Info("loading");
+        }
+
+        /// <summary>
+        /// Called by the game when the game world is created.
+        /// </summary>
+        /// <param name="updateSystem">Game update system.</param>
+        public void OnCreateWorld(UpdateSystem updateSystem)
+        {
+            Log.Info("starting OnCreateWorld");
+
+            // Add mod UI resource directory to UI resource handler.
+            GameUIResourceHandler uiResourceHandler = GameManager.instance.userInterface.view.uiSystem.resourceHandler as GameUIResourceHandler;
+            uiResourceHandler?.HostLocationsMap.Add("uil", new System.Collections.Generic.List<string> { Mod.Instance.AssemblyPath + "/Standard" });
+        }
+
+        /// <summary>
+        /// Called by the game when the mod is disposed of.
+        /// </summary>
+        public void OnDispose()
+        {
+            Log.Info("disposing");
+            Instance = null;
+        }
+    }
+}
